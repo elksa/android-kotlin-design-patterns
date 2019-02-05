b18afd22315d1ed6fe6c47f307acd0e2489c64cc
Alejandro Casanova Mutis <elksa2003@gmail.com>
2019-02-05 15:10:10 -0500

    Added missing pattern activities

---
+++ app/src/main/java/com/elksa/design/patterns/activities/GofDecoratorActivity.java
@@ -0,0 +1,15 @@
+package com.elksa.design.patterns.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elksa.design.patterns.R;
+
+public class GofDecoratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gof_decorator);
+    }
+}
